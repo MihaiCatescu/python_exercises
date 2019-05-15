451a0f8dc42106deda98d16e30bd069f1fa98e89
Mihai Catescu <mihai.c.catescu@gmail.com>
2019-05-16 00:20:35 +0300

    Update exercise22.py

--- exercise22.py
+++ exercise22.py
@@ -16,3 +16,16 @@ SeriesSum(5) => 1 + 1/4 + 1/7 + 1/10 + 1/13 = "1.57"
 '''
 
 def series_sum(n):
+    series = []
+    denominator = 4
+    if n == 0:
+        return '%.2f' % n
+    elif n == 1:
+        return '%.2f' % n
+    elif n > 1:
+        series.append(1)
+        while len(series) < n:
+            series.append(1 / denominator)
+            denominator += 3
+        total = sum(series)
+        return '%.2f' % total
